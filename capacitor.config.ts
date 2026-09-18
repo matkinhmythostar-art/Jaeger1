6539e7da970d94f9f60a609196974bff5faa45e4
matkinhmythostar-art <matkinhmythostar@gmail.com>
2026-09-18 08:51:52 +0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.matkinhmytho.jaeger',
   appName: 'JaegerVisionPro',
-  webDir: '.',
+  webDir: 'www'
 };
 
 export default config;
